f3bfcd4ef8a76f06dea5a83008bca318c4c5e883
najeebmahajne <najeebmahajne7@hotmail.com>
2023-06-04 12:04:19 +0300

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,9 +4,14 @@ import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setOrigin,setDestination} from '../slices/navSlice';
 
 const imageUrl = "https://links.papareact.com/gzs";
 const HomeScreen = () => {
+
+  const dispatch=useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -29,10 +34,14 @@ const HomeScreen = () => {
             fontSize: 18,
           },
         }}
-        onPress={(data, details=null) => {
-          console.log(data);
-          console.log(details);
-        } }
+        onPress={ (data, details = null) => {
+          dispatch( setOrigin({
+            location: details.geometry.location,
+            description: data.description,
+          })
+          );
+          dispatch(setDestination(null));
+        }}
         minLength={2}
         enablePoweredByContainer={false}
         query={
@@ -41,6 +50,7 @@ const HomeScreen = () => {
             language: "en",
           }
         }
+        returnKeyType={"search"}
         fetchDetails={true}
         nearbyPlacesAPI='GooglePlacesSearch'
         debounce={400}
